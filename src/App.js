61126d65485ea0d5bb7de8fe3b90d76c6fac75ed
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-26 17:08:30 +0300

    Add lower navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <h1>We are adding something new. also</h1>
+    <div>
+      <Navigation />
     </div>
   );
 }
