10830309426ab6407f23cde12ca4dfe693ae2a57
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-26 19:36:12 +0300

    start banner section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Navigation from './components/Navigation';
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="d-xl-flex">
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <img src="./assets/bannerImage.png" alt="banner image" />
+    </div>
+  );
+}
+
+export default Banner;
