f963b5509e8b6ddefa16557e0964718e7cc6e828
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-26 16:43:03 +0300

    new check

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>We are adding something new</h1>
+      <h1>We are adding something new. also</h1>
     </div>
   );
 }
