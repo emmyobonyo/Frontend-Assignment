f8fc16774927323b27a493c321a3a1c0df34d143
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-27 12:27:14 +0300

    Finish banner section for desktop screens

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,14 +2,52 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="d-xl-flex">
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="d-xl-flex justify-content-between banner__section">
+      <div className="mt-5 banner__left">
+        <div className="d-xl-flex justify-content-between">
+          <div className="d-flex flex-column">
+            <h3 className="banner__year">2022</h3>
+            <p className="applications">APPLICATIONS</p>
+          </div>
+          <div className="d-flex justify-content-center align-items-center mt-4 now__open p-5">
+            NOW OPEN
+          </div>
+        </div>
+        <p className="banner__paragraph">
+          Choose from our enrollment forms to join our college
+        </p>
+        <div>
+          <div>
+            <img className="checkmark" src="./assets/checkmark.svg" />
+            <span className="banner__descriptions">
+              A Student's First Choice
+            </span>
+          </div>
+          <div>
+            <img className="checkmark" src="./assets/checkmark.svg" />
+            <span className="banner__descriptions">
+              Opening Doors to the Future
+            </span>
+          </div>
+          <div>
+            <img className="checkmark" src="./assets/checkmark.svg" />
+            <span className="banner__descriptions">Top Ranked Lecturers</span>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between banner__button">
+          <span>Button</span>
+          <img src="./assets/arrow-button.svg" />
+        </div>
+        <div className="mt-3">
+          <img src="./assets/play-filled.svg" />
+          <span className="lorem__ipsum">Lorem ipsum </span>
+        </div>
       </div>
-      <img src="./assets/bannerImage.png" alt="banner image" />
+      <img
+        className="w-50 h-50"
+        src="./assets/bannerImage.png"
+        alt="banner image"
+      />
     </div>
   );
 }
