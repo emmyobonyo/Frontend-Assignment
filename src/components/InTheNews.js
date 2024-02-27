d22dec9abf41c311d9ef0a06f1c3acdd0200f015
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-27 13:18:13 +0300

    add a separator

---
+++ src/components/InTheNews.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InTheNews() {
+  return (
+    <div>
+      <div class=" d-none d-md-block separator"></div>
+    </div>
+  );
+}
+
+export default InTheNews;
