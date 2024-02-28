e56566461973d018fc6d289cfc635c23d091ef1d
Emmanuel Obonyo <emmyobonyo@gmail.com>
2024-02-28 16:56:42 +0300

    add dynamic cards to section

--- src/components/Programmes.js
+++ src/components/Programmes.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import programmes from "../data/programmes";
+import programDetails from "../data/programDetails";
 
 function Programmes() {
-  const programmesOffered = programmes.map((programme) => (
+  const [state, setState] = useState(0);
+  const changeContents = (index) => {
+    setState(index);
+  };
+  const programmesOffered = programmes.map((programme, index) => (
     <div>
       <div className="programme__image">
-        <img className="w-75 h-75" src={programme.image} />
+        <img
+          className="w-75 h-75"
+          src={programme.image}
+          onClick={() => changeContents(index)}
+        />
       </div>
       <p className="mt-4 programme__text">{programme.text}</p>
     </div>
@@ -23,6 +32,19 @@ function Programmes() {
         </div>
         <div className="d-flex flex-column flex-md-row justify-content-between mt-5">
           {programmesOffered}
+        </div>
+        <div class="bg-white mt-5">
+          <div class="d-none d-md-flex .bg-white p-3">
+            <img class="w-50 h-50" src={programDetails[state].image} />
+            <div>
+              <h3 class="w-75 mx-auto fw-normal">
+                You have an engineering vision. We can make it happen
+              </h3>
+              <p class="mt-5 w-75 mx-auto">
+                {programDetails[state].description}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/data/programDetails.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    image: "./assets/engineering.jpg",
+    description:
+      "Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet",
+  },
+  {
+    image: "./assets/construction.jpg",
+    description:
+      "Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet",
+  },
+  {
+    image: "./assets/engineering.jpg",
+    description:
+      "Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet",
+  },
+  {
+    image: "./assets/construction.jpg",
+    description:
+      "Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet Lorem ispsum idio sit amet",
+  },
+];
